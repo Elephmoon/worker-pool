be74a9f0d2756d21121569309668c8266c21805f
Elephmoon <alexandr.kizilow@gmail.com>
2021-01-05 16:09:41 +0300

    add run pool in Background

--- workerpool/pool.go
+++ workerpool/pool.go
@@ -1,12 +1,17 @@
 package workerpool
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type Pool struct {
-	Tasks       []*Task
-	concurrency int
-	collector   chan *Task
-	wg          sync.WaitGroup
+	Tasks         []*Task
+	Workers       []*Worker
+	runBackground chan struct{}
+	concurrency   int
+	collector     chan *Task
+	wg            sync.WaitGroup
 }
 
 func NewPool(tasks []*Task, concurrency int) *Pool {
@@ -28,4 +33,31 @@ func (p *Pool) Run() {
 	}
 	close(p.collector)
 	p.wg.Wait()
+}
+
+func (p *Pool) AddTask(task *Task) {
+	p.collector <- task
+}
+
+func (p *Pool) RunBackground() {
+	go func() {
+		time.Sleep(time.Millisecond)
+	}()
+	for i := 0; i < p.concurrency; i++ {
+		worker := NewWorker(p.collector, i)
+		p.Workers = append(p.Workers, worker)
+		go worker.StartBackground()
+	}
+	for _, task := range p.Tasks {
+		p.collector <- task
+	}
+	p.runBackground = make(chan struct{})
+	<-p.runBackground
+}
+
+func (p *Pool) StopBackground() {
+	for _, worker := range p.Workers {
+		worker.StopBackground()
+	}
+	p.runBackground <- struct{}{}
 }
--- workerpool/worker.go
+++ workerpool/worker.go
@@ -3,14 +3,16 @@ package workerpool
 import "sync"
 
 type Worker struct {
-	ID       int
-	taskChan chan *Task
+	ID         int
+	taskChan   chan *Task
+	finishChan chan struct{}
 }
 
 func NewWorker(taskChan chan *Task, ID int) *Worker {
 	return &Worker{
-		ID:       ID,
-		taskChan: taskChan,
+		ID:         ID,
+		taskChan:   taskChan,
+		finishChan: make(chan struct{}),
 	}
 }
 
@@ -22,4 +24,19 @@ func (w *Worker) Start(wg *sync.WaitGroup) {
 			process(task)
 		}
 	}()
+}
+
+func (w *Worker) StartBackground() {
+	for {
+		select {
+		case task := <-w.taskChan:
+			process(task)
+		case <-w.finishChan:
+			return
+		}
+	}
+}
+
+func (w *Worker) StopBackground() {
+	w.finishChan <- struct{}{}
 }
